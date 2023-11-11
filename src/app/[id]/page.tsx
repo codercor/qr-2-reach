e0c7449c6d1549457c30e3fc49c3aa7aeeaf96cf
Mustafa Çor <mmustafaa9@gmail.com>
2023-11-11 22:31:07 +0300

    Destoy the project �

--- src/app/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import {useParams} from "next/navigation";
-
-const ProfilePage = () => {
-    const {id} = useParams<{ id: string }>();
-    return <div>Profile Page {id}</div>;
-}
-
-
-export default ProfilePage;
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="min-h-screen flex flex-col items-center justify-between bg-amber-200">
-
-    </main>
-  )
-}
