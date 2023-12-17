edaa57881be230def01d813d9badc91f3eb11f9d
codercor <mmustafaa9@gmail.com>
2023-12-17 19:50:11 +0300

    clear

--- prisma/migrations/20231010221503_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "phone" TEXT NOT NULL DEFAULT '+905005554433';
--- src/app/[qr]/components/RegisterProfile.tsx
+++
@@ -1,7 +0,0 @@
-const RegisterProfile = () => {
-    return <>
-     <h1> Register Page </h1>
-    </>
-}
-
-export default RegisterProfile;
